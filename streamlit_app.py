621e834a6e1667a0321c79e4c4ccbf223da2a348
sfdmsdabw <131622999+sfdmsdabw@users.noreply.github.com>
2023-04-25 09:47:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,11 +84,18 @@ if streamlit.button('Get Fruit Load List'):
     
 ############################
 
-streamlit.stop()
 
-fruit_to_add = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', fruit_to_add)
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        return "Thanks for adding " + new_fruit
+    
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+fruit_to_add = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_func = insert_row_snowflake(fruit_to_add)
+    streamlit.text(back_from_func)
 
 
+#streamlit.stop()
